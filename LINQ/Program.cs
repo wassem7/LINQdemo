1dad6fad76b87f99b5ba51fa6e88b19e3c64b71c
wassem7 <waseemdarkwa@gmail.com>
2023-11-27 17:46:39 +0000

    FirstorDefault / LastorDefault

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace LINQ
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            var people = ReadPeopleData();
+
+            var firstPerson = people.FirstOrDefault();
+            var lastPerson = people.LastOrDefault();
+            Console.WriteLine("***** First Person *****");
+            Console.WriteLine(firstPerson);
+            Console.WriteLine();
+            Console.WriteLine("- - - - - - - - - - - - - - - - ");
+            Console.WriteLine();
+            Console.WriteLine("***** Last Person *****");
+            Console.WriteLine(lastPerson);
+        }
+
+        static Person[] ReadPeopleData()
+        {
+            using (var reader = new StreamReader("people.json"))
+            {
+                string jsondata = reader.ReadToEnd();
+                var people = JsonConvert.DeserializeObject<Person[]>(jsondata);
+                return people;
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public DateTime Birthdate { get; set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID-[{Id}] | {FirstName} {LastName} | {City} | {Salary} | {Birthdate.ToShortDateString()}";
         }
     }
 }
